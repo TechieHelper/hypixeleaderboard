f77895d806122341089242c5151c97b00223ed54
TechieHelper <oliverlford@icloud.com>
2020-08-13 21:27:40 +0100

    Custom Error Pages

--- main.py
+++ main.py
@@ -294,5 +294,25 @@ def customReturn(data, dataPoints):
     return newData
 
 
+@app.errorhandler(403)
+def error_403(e):
+    return render_template("403.html"), 403
+
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(405)
+def error_405(e):
+    return render_template("405.html"), 405
+
+
+@app.errorhandler(500)
+def error_500(e):
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     app.run()
