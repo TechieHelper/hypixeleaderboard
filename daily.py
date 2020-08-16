07b369886db8c01b907a936a626d91a00b8fbcf4
TechieHelper <oliverlford@icloud.com>
2020-08-16 20:50:48 +0100

    Daily Resets

---
+++ daily.py
@@ -0,0 +1,7 @@
+import urllib.request, urllib.error, urllib.parse
+URL_EXTENTIONS = ["bedwars", "skywars", "duels", "arena", "mcgo", "battleground", "survival-games", "uhc", "walls", "paintball", "murder-mystery", "super-smash", "speed-uhc", "tntgames", "gingerbread", "build-battle", "arcade", "skyclash", "quakecraft", "true-combat", "walls3", "vampirez"]
+for extention in URL_EXTENTIONS:
+    response = urllib.request.urlopen("http://www.hypixeleaderboards.com/leaderboards/" + extention)
+
+with open("./knownUsers.json", "w") as f:
+    f.write("{}")
