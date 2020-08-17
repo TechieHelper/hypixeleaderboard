ee0734226ea1a9136785af365c150797685973f1
TechieHelper <oliverlford@icloud.com>
2020-08-17 09:24:16 +0100

    Dumb daily.py fix

--- daily.py
+++ daily.py
@@ -1,7 +1,8 @@
 import urllib.request, urllib.error, urllib.parse
 URL_EXTENTIONS = ["bedwars", "skywars", "duels", "arena", "mcgo", "battleground", "survival-games", "uhc", "walls", "paintball", "murder-mystery", "super-smash", "speed-uhc", "tntgames", "gingerbread", "build-battle", "arcade", "skyclash", "quakecraft", "true-combat", "walls3", "vampirez"]
-for extention in URL_EXTENTIONS:
-    response = urllib.request.urlopen("http://www.hypixeleaderboards.com/leaderboards/" + extention)
 
 with open("./knownUsers.json", "w") as f:
     f.write("{}")
+
+for extention in URL_EXTENTIONS:
+    response = urllib.request.urlopen("http://www.hypixeleaderboards.com/leaderboards/" + extention)
